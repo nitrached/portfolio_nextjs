66067403dc23a1b8e9df648c280143d25576a22d
nitrached <manech.dubreil@epitech.eu>
2024-02-22 16:54:57 +0100

    project

---
+++ src/app/page.jsx
@@ -0,0 +1,46 @@
+"use client";
+
+import Image from "next/image";
+import { motion } from "framer-motion";
+import Link from "next/link";
+
+const Homepage = () => {
+  return (
+    <motion.div
+      className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    >
+      <div className="h-full w-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+        {/* TEXT CONTAINER */}
+        <div className="lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
+          {/* TITLE */}
+          <h1 className="text-4xl md:text-6xl font-bold">
+            Crafting Digital Experiences, Designing Tomorrow.
+          </h1>
+          {/* DESC */}
+          <p className="md:text-xl">
+            Welcome to my digital canvas, where my
+            portfolio showcases a diverse collection of my projects.
+          </p>
+          {/* BUTTONS */}
+          <div className="w-full flex gap-4">
+            <Link href="/portfolio">
+            <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
+              View My Work
+            </button>
+            </Link>
+            <Link href="/contact">
+            <button className="p-4 rounded-lg ring-1 ring-black">
+              Contact Me
+            </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Homepage;
